fd21d4a6e53c241f575cf984c093ddc7e816de9e
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-15 01:01:05 +0530

    ReadingDataFromTextFile

--- ReadingDataFromTextFile.cs
+++ ReadingDataFromTextFile.cs
@@ -10,17 +10,17 @@ namespace File_IO
     {
         public static void Main(string[] args)
         {
-            //@-In C#, a verbatim string is created using a special symbol @.
-            //@ is known as a verbatim identifier.
+            // @-In C#, a verbatim string is created using a special symbol @.
+            // @ is known as a verbatim identifier.
             string path = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\FileIO.txt";
 
-            //(File.Exists)- method is used to check that a particular file exists or not.
+            // (File.Exists)- method is used to check that a particular file exists or not.
             if (File.Exists(path))
             {
                 Console.WriteLine("Yes, the file exists");
 
-                //ReadAllText(String) is used for Opens a text file, reads all the text in the file,
-                //and then closes the file.
+                // ReadAllText(String) is used for Opens a text file, reads all the text in the file,
+                // and then closes the file.
                 string data = File.ReadAllText(path);
                 Console.WriteLine(data);
             }
