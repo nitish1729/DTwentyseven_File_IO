94a6487bc986e66d4b59c9e4530cac0f44dcee46
Nitish Mehta <nkrmehta09@gmail.com>
2022-07-15 03:39:33 +0530

    Create Copy of file

---
+++ CreateCopyOfFile.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.IO; // This Namespace is used for performing operations with files
+
+//namespace File_IO
+//{
+//    public class CreateCopyOfFile
+//    {
+//        public static void Main(string[] args)
+//        {
+//            string path = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\FileIO.txt";
+
+//            // copy the text file inside the same folder with different name
+//            string path1 = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\FileIO2.txt";
+
+//            // To copy the file inside the folder 
+//            string path2 = @"C:\Users\NITISH MEHTA\OneDrive\Desktop\Bridge_Labz\27\Data\FileIO3.txt";
+//            // File.Copy(path, path2); for copy with differ file name or in differ folder
+
+//            File.Copy(path, path2,true); //
+//            Console.ReadLine();
+//        }
+//    }
+//}
